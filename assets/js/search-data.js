ab38c734a1afb9a951bcf3813051571c20902027
danaibrilli <36965213+danaibrilli@users.noreply.github.com>
2025-04-15 11:40:25 +0000

    Deploying to gh-pages from @ danaibrilli/danaibrilli.github.io@0ffe03a586c23c580b4ef19d88d9b2a351027a0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,9 +409,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-glioblastoma-pseudoprogression-detection",
+          title: 'Glioblastoma Pseudoprogression Detection',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
