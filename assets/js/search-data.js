b7db288e4effff905b277e168d5bc23d26338d0e
danaibrilli <36965213+danaibrilli@users.noreply.github.com>
2025-04-15 12:10:54 +0000

    Deploying to gh-pages from @ danaibrilli/danaibrilli.github.io@b957c229bbff2e600b44190ec45b3975534bfe93 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-gsoc",
+          title: 'GSoC',
+          description: "Google Summer of Code",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
